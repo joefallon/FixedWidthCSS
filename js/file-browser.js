af43a9c5a9ab27bc210ad058025fa79f86300b85
Joe Fallon <joseph.t.fallon@gmail.com>
2014-10-12 21:44:48 -0700

    Most buttons were completed. File chooser button was started.

---
+++ js/file-browser.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+$(document).ready( function() {
+    $(".btn-file :file").on("fileselect", function(event, numFiles, label) {
+        console.log(numFiles);
+        console.log(label);
+    });
+});
